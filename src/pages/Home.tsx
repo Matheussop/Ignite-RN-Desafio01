7d280a1e096d113947812b4ff1ebd7a7ef22bf64
Matheus Luiz Oliveira Spindula <36748285+Matheussop@users.noreply.github.com>
2021-05-27 17:34:11 -0300

    conclusão do primeiro desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,42 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task if it's not empty
+    if(newTaskTitle || newTaskTitle != ""){
+      const data: Task = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false
+      }
+      setTasks((oldState: Task[]) => [...oldState, data]);
+    }
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    // Não é necessário verificar se a task existe, poís este método so ira ativar 
+    // quando o usuário clicar na task (ou seja ela precisa existir para que o método 
+    // seja acionado)
+    var newlistTask: Task[] = [];
+    tasks.map((task) => {
+      if(task.id === id){
+        var auxTasks = task;
+        auxTasks.done = !auxTasks.done;
+        newlistTask.push(auxTasks)
+      }else{
+        newlistTask.push(task)
+      }
+    })
+    setTasks(newlistTask)
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks((oldState) => oldState.filter(skill =>
+      skill.id !== id
+    ))
   }
 
   return (
